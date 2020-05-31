53e297771173ece2dbe383ac3a9f5eaaf62aef07
Nicolas Imperatori <nico.794@gmail.com>
2020-05-31 19:48:40 -0300

    add playing time

--- codigo/app/src/main/java/com/tpsoa/ListAdapter.kt
+++ codigo/app/src/main/java/com/tpsoa/ListAdapter.kt
@@ -1,7 +1,9 @@
 package com.tpsoa
 
+import android.R.attr.duration
 import android.content.Context
 import android.media.MediaPlayer
+import android.os.Handler
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -11,14 +13,16 @@ import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
 import com.tpsoa.model.VoiceNote
-import android.os.Handler
+import java.io.File
+import java.lang.String
+import java.util.concurrent.TimeUnit
 
 
 class ListAdapter(var cont: Context, var resource: Int, var items: List<VoiceNote>) :
     ArrayAdapter<VoiceNote>(cont, resource, items) {
 
     //    private lateinit var player: MediaPlayer
-    private lateinit var startTime: TextView
+//    private lateinit var startTime: TextView
     private lateinit var songTime: TextView
 
     //    private lateinit var seekBar: SeekBar
@@ -43,26 +47,42 @@ class ListAdapter(var cont: Context, var resource: Int, var items: List<VoiceNot
 
         val view: View = layoutInflater.inflate(resource, null)
         var nameTextView = view.findViewById<TextView>(R.id.name)
+        var durationTextView = view.findViewById<TextView>(R.id.duration)
         var locationTextView = view.findViewById<TextView>(R.id.location)
+        var timeTextView = view.findViewById<TextView>(R.id.time)
         var playBtn = view.findViewById<Button>(R.id.playBtn)
 
         var seekBar = view.findViewById<SeekBar>(R.id.seekBar)
 
-        startTime = view.findViewById(R.id.txtStartTime)
-        songTime = view.findViewById(R.id.txtSongTime)
 
         var voiceNote: VoiceNote = items[position]
 
-        nameTextView.text = voiceNote.name + " " + voiceNote.duration.time
-        locationTextView.text = "location"
+        var file = File(voiceNote.path)
 
         var player = MediaPlayer()
+        player.setDataSource(voiceNote.path)
+        player.prepare()
+
+        var duration = String.format(
+            "%02d:%02d",
+            TimeUnit.MILLISECONDS.toMinutes(player.duration.toLong()),
+            TimeUnit.MILLISECONDS.toSeconds(player.duration.toLong()) -
+                    TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(player.duration.toLong()))
+        )
+
+        nameTextView.text = file.name
+        durationTextView.text = duration
+        locationTextView.text = voiceNote.location
+
 
         player.setOnCompletionListener { player ->
             playBtn.text = "play"
             Log.i("asd", "stoping " + voiceNote.path)
-            player.reset()
+//            player.reset()
             seekBar.progress = 0
+            timeTextView.text = "00:00"
+            player.seekTo(0)
+            handler.removeCallbacks(runnable)
         }
 
         playBtn.setOnClickListener {
@@ -76,24 +96,28 @@ class ListAdapter(var cont: Context, var resource: Int, var items: List<VoiceNot
                 Log.i("asd", "playing " + voiceNote.path)
                 playBtn.text = "pause"
 
-                try {
+//                try {
 //                    player = MediaPlayer()
-                    player.setDataSource(voiceNote.path)
-                    player.prepare()
-                } catch (e: IllegalArgumentException) {
-                    e.printStackTrace()
-                } catch (e: Exception) {
-                    println("Exception of type : $e")
-                    e.printStackTrace()
-                }
+//                    player.setDataSource(voiceNote.path)
+//                    player.prepare()
+//                } catch (e: IllegalArgumentException) {
+//                    e.printStackTrace()
+//                } catch (e: Exception) {
+//                    println("Exception of type : $e")
+//                    e.printStackTrace()
+//                }
                 /////////////////////////////////////////////////
                 seekBar.max = player.seconds
                 runnable = Runnable {
                     seekBar.progress = player.currentSeconds
 
-                    startTime.text = "${player.currentSeconds} sec"
-                    val diff = player.seconds - player.currentSeconds
-                    songTime.text = "$diff sec"
+                    var current = String.format(
+                        "%02d:%02d",
+                        TimeUnit.MILLISECONDS.toMinutes(player.currentPosition.toLong()),
+                        TimeUnit.MILLISECONDS.toSeconds(player.currentPosition.toLong()) -
+                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(player.currentPosition.toLong()))
+                    )
+                    timeTextView.text = current
 
                     handler.postDelayed(runnable, 1000)
                 }
@@ -104,6 +128,20 @@ class ListAdapter(var cont: Context, var resource: Int, var items: List<VoiceNot
             Log.i("asd", "isPlaying: " + player.isPlaying.toString())
             Log.i("asd", "---------------------------")
         }
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                if (b) {
+                    player.seekTo(i * 1000)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
+        })
 
         return view
     }
